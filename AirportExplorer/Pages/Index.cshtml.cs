e918399f40e93544dfb08be9c97973e1fc7c9e0a
ferrnavarro <fer.maganavarro@gmail.com>
2019-08-04 22:47:24 -0600

    Showing details card

--- AirportExplorer/Pages/Index.cshtml.cs
+++ AirportExplorer/Pages/Index.cshtml.cs
@@ -11,6 +11,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using GoogleApi;
+using GoogleApi.Entities.Common;
+using GoogleApi.Entities.Common.Enums;
+using GoogleApi.Entities.Places.Details.Request;
+using GoogleApi.Entities.Places.Photos.Request;
+using GoogleApi.Entities.Places.Search.NearBy.Request;
 
 namespace AirportExplorer.Pages
 {
@@ -18,11 +24,14 @@ namespace AirportExplorer.Pages
     {
         private readonly IHostingEnvironment _hostingEnvironment;
         public string MapboxAccessToken { get;  }
+        public string GoogleApiKey { get; set; }
+
 
         public IndexModel(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
         {
             _hostingEnvironment = hostingEnvironment;
             MapboxAccessToken = configuration["Mapbox:AccessToken"];
+            GoogleApiKey = configuration["google:ApiKey"];
         }
 
         public void OnGet()
@@ -56,7 +65,7 @@ namespace AirportExplorer.Pages
                         new Dictionary<string, object>
                         {
                             {"name", name },
-                            {"iatCode", iataCode }
+                            {"iataCode", iataCode }
                         }));
 
                 }
@@ -64,5 +73,73 @@ namespace AirportExplorer.Pages
                 return new JsonResult(featureCollection);
             }                              
         }
+
+
+        public async Task<IActionResult> OnGetAirportDetail(string name, double latitude, double longitude)
+        {
+            var airportDetail = new AirportDetail();
+
+            var searchResponse = await GooglePlaces.NearBySearch.QueryAsync(new PlacesNearBySearchRequest
+            {
+                Key = GoogleApiKey,
+                Name = name,
+                Location = new Location(latitude, longitude),
+                Radius = 1000
+            });
+
+            if (!searchResponse.Status.HasValue || searchResponse.Status.Value != Status.Ok || !searchResponse.Results.Any())
+                return new BadRequestResult();
+
+            var nearbyResult = searchResponse.Results.FirstOrDefault();
+            string placeId = nearbyResult.PlaceId;
+            string photoReference = nearbyResult.Photos?.FirstOrDefault()?.PhotoReference;
+            string photoCredit = nearbyResult.Photos?.FirstOrDefault()?.HtmlAttributions.FirstOrDefault();
+
+            var detailResponse = await GooglePlaces.Details.QueryAsync(new PlacesDetailsRequest
+            {
+                Key = GoogleApiKey,
+                PlaceId = placeId
+            });
+
+            if (!detailResponse.Status.HasValue || detailResponse.Status.Value != Status.Ok)
+                return new BadRequestResult();
+
+            var detailsResult = detailResponse.Result;
+            airportDetail.FormattedAddress = detailsResult.FormattedAddress;
+            airportDetail.PhoneNumber = detailsResult.InternationalPhoneNumber;
+            airportDetail.Website = detailsResult.Website;
+
+            if (photoReference != null)
+            {
+                var photosResponse = await GooglePlaces.Photos.QueryAsync(new PlacesPhotosRequest
+                {
+                    Key = GoogleApiKey,
+                    PhotoReference = photoReference,
+                    MaxWidth = 400
+                });
+
+                if (photosResponse.Buffer != null)
+                {
+                    airportDetail.Photo = Convert.ToBase64String(photosResponse.Buffer);
+                    airportDetail.PhotoCredit = photoCredit;
+                }
+            }
+
+            return new JsonResult(airportDetail);
+
+        }
+
     }
+
+
+    public class AirportDetail
+    {
+        public string FormattedAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Photo { get; set; }
+        public string PhotoCredit { get; set; }
+        public string Website { get; set; }
+    }
+
+
 }
